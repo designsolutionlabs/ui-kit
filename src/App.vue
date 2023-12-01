ca87f57a6fd87f6351fd8832b5a6db453120b3fd
bagasw980 <bagasw980@gmail.com>
2023-12-01 14:30:52 +0700

    fisrt commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h3>Hello World</h3>
+  </div>
+</template>
+
+<style lang="scss">
+@import "./assets/index.scss";
+.box {
+  width: 200px;
+  height: 200px;
+  background-color: $colorBackgroundBgFilledSuccess;
+}
+</style>
